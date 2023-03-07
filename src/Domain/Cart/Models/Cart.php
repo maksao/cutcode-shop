35c47b26e24bf6a498d643bbdc4dbe3ee6db754e
Maks <maksm@avto-olimp.com>
2023-03-07 13:11:08 +0300

    stage 7 done

--- src/Domain/Cart/Models/Cart.php
+++ src/Domain/Cart/Models/Cart.php
@@ -3,12 +3,14 @@
 namespace Domain\Cart\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\MassPrunable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
     use HasFactory;
+    use MassPrunable;
 
     protected $fillable = [
         'storage_id',
@@ -18,5 +20,10 @@ class Cart extends Model
     public function cartItems(): HasMany
     {
         return $this->hasMany(CartItem::class);
+    }
+
+    public function prunable(): Cart
+    {
+        return static::where('created_at', '<=', now()->subDay());
     }
 }
